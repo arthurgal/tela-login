61738b719870aa2ccda3171501927867371902c9
arthurgal <arthurgaldinoo@hotmail.com>
2022-11-25 12:52:33 -0300

    refazendo formulario

--- src/components/FormularioEquipamento.vue
+++ src/components/FormularioEquipamento.vue
@@ -1,16 +1,24 @@
 <template>
-    <div>
-        <b-form>
+    <div class="w-50 m-auto p-5">
+        <h2 class="text-center mb-4">Cadastro de Equipamentos</h2>
+        <b-form >
             <b-form-group id="input-group-1" label="Nome" label-for="input-1"
                 description="Diga qual equipamento você quer cadastrar">
-                <b-form-input id="input-1" v-model="formulario.nome" type="email" require>
+                <b-form-input id="input-1" v-model="formulario.nome" type="text" require>
 
                 </b-form-input>
             </b-form-group>
 
-            <b-form-group id="input-group-1" label="Modelo" label-for="input-1"
+            <b-form-group id="input-group-2" label="Modelo" label-for="input-2"
                 description="Diga o modelo do equipamento">
-                <b-form-input id="input-1" v-model="formulario.modelo" type="email" require>
+                <b-form-input id="input-1" v-model="formulario.modelo" type="text" require>
+
+                </b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-3" label="Modelo" label-for="input-3"
+                description="Diga o modelo do equipamento">
+                <b-form-input id="input-1" v-model="formulario.modelo" type="text" require>
 
                 </b-form-input>
             </b-form-group>
--- src/components/MenuPrincipal.vue
+++ src/components/MenuPrincipal.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="side-bar">
+    <div class="side-bar h-100 w-75">
         <h3>Cadastro</h3>
         <b-nav vertical class="text-start">
-            <b-nav-item active><b-icon icon="house-door-fill"></b-icon>Inicio</b-nav-item>
+            <b-nav-item active><b-icon icon="house-door-fill"></b-icon>Equipamento</b-nav-item>
             <b-nav-item active><b-icon icon="person-fill"></b-icon>Usuarios</b-nav-item>
-            <b-nav-item active><b-icon icon="wallet-fill"></b-icon>Equipamentos</b-nav-item>
+            <b-nav-item active><b-icon icon="wallet-fill"></b-icon>Setor</b-nav-item>
+            <b-nav-item active><b-icon icon="wallet-fill"></b-icon>Sala</b-nav-item>
+            <b-nav-item active><b-icon icon="wallet-fill"></b-icon>Predio</b-nav-item>
+            <b-nav-item active><b-icon icon="wallet-fill"></b-icon>Status Equipamento</b-nav-item>
+            <b-nav-item active><b-icon icon="wallet-fill"></b-icon>Tipo Equipamento</b-nav-item>
         </b-nav>
     </div>
 </template>
@@ -20,8 +24,6 @@ export default {
 .side-bar {
     -webkit-box-shadow: 4px 0px 20px -11px #000000;
     box-shadow: 4px 0px 20px -11px #000000;
-    width: 75%;
-    height: 100vh;
     background-color: #384c61;
 }
 

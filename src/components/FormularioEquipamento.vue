d2040a83e48215dce63acdaf1f9b26f390524c4d
Arthur Galdino <arthurgaldinoo@hotmail.com>
2022-11-27 13:31:33 -0300

    Criando primeira linha do formulario de Equipamentos

--- src/components/FormularioEquipamento.vue
+++ src/components/FormularioEquipamento.vue
@@ -1,43 +1,87 @@
 <template>
-    <b-container class="w-50 m-auto p-5">
-        <h2 class="text-center mb-4">Cadastro de Equipamentos</h2>
-        <b-form >
-            <b-form-group id="input-group-1" label="Nome" label-for="input-1"
-                description="Diga qual equipamento você quer cadastrar">
-                <b-form-input id="input-1" v-model="formulario.nome" type="text" require>
-
-                </b-form-input>
+  <b-container class="w-80 m-auto pt-5">
+    <b-row>
+      <h2 class="mb-4">Cadastro de Novo Equipamentos</h2>
+    </b-row>
+    <b-row>
+      <b-form>
+        <b-row>
+          <b-col cols="4">
+            <b-form-group
+              id="input-group-1"
+              label="Tombo"
+              label-for="input-1"
+        
+            >
+              <b-form-input
+                id="input-1"
+                v-model="formulario.nome"
+                type="text"
+                require
+              ></b-form-input>
             </b-form-group>
 
-            <b-form-group id="input-group-2" label="Modelo" label-for="input-2"
-                description="Diga o modelo do equipamento">
-                <b-form-input id="input-1" v-model="formulario.modelo" type="text" require>
-
-                </b-form-input>
+          </b-col>
+          <b-col cols="4">
+            <b-form-group
+              id="input-group-"
+              label="Tipo Equipamento:"
+              label-for="input-2"
+            >
+              <b-form-select class="w-100 p-2"
+                id="input-2"
+                v-model="formulario.tipoEquipamento"
+                :options="tipoEquipamento"
+                required
+              ></b-form-select>
             </b-form-group>
+          </b-col>
 
-            <b-form-group id="input-group-3" label="Modelo" label-for="input-3"
-                description="Diga o modelo do equipamento">
-                <b-form-input id="input-1" v-model="formulario.modelo" type="text" require>
-
-                </b-form-input>
+        <b-col cols="4">
+            <b-form-group
+              id="input-group-"
+              label="Status (Condição):"
+              label-for="input-3"
+            >
+              <b-form-select class="w-100 p-2"
+                id="input-3"
+                v-model="formulario.status"
+                :options="status"
+                required
+              ></b-form-select>
             </b-form-group>
-        </b-form>
-    </b-container>
+          </b-col>
+
+        </b-row>
+      </b-form>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-    export default {
-        name:'FormularioEquipamento',
-        data(){
-            return{
-                formulario:{
-                    nome:'',
-                    modelo:''
-
-                }
-                
-            }
-        }
-    }
+export default {
+  name: "FormularioEquipamento",
+  data() {
+    return {
+      formulario: {
+        nome: "",
+        tipoEquipamento: null,
+        status: null
+      },
+      tipoEquipamento: [
+        { text: "Selecione", value: null },
+        "Computador",
+        "Notebook",
+        "Monitor",
+        "Estabilizaor",
+      ],
+      status: [
+        { text: "Selecione", value: null },
+        "Funcionando",
+        "Quebrado",
+        "Ocioso"
+      ],
+    };
+  },
+};
 </script>

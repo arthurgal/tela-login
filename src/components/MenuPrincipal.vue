ca1f0e8f1016b81d92bc92cbc026ef2db51db1f5
Arthur Galdino <arthurgaldinoo@hotmail.com>
2022-11-28 22:47:00 -0300

    Melhorando o menu principal

--- src/components/MenuPrincipal.vue
+++ src/components/MenuPrincipal.vue
@@ -1,10 +1,15 @@
 <template>
-    <div class="side-bar">
-        <div class="d-flex header-menu">
-            <img class="imagem-almoxarifado" src="../../public/img/almoxarifado.png">
-            <h3>Cadastro</h3>
-        </div>
-        <b-nav justified vertical>
+    <b-container class="side-bar">
+        <b-row class="pt-3 pb-2 mb-3 border-bottom">
+            <b-col cols="auto">
+                <img class="imagem-almoxarifado" src="../../public/img/almoxarifado.png">
+            </b-col>
+            <b-col cols="auto" class="mt-2">
+                <h3>Cadastro</h3>
+            </b-col>
+        </b-row>
+        
+        <b-nav vertical>
             <div class="d-flex align-items-center div-tipicos">
                 <b-icon variant="light" icon="arrow-right-circle"></b-icon>
                 <b-nav-item to="/equipamento" active>Equipamento</b-nav-item>
@@ -41,7 +46,7 @@
             </div>
 
         </b-nav>
-    </div>
+    </b-container>
 </template>
 
 <script>
@@ -59,16 +64,7 @@ export default {
     height: 735px;
 }
 
-.header-menu{
-    padding-top: 10px;
-    padding-left: 30px;
-    border-bottom: #2d3c4c solid 2px;
-}
-
 .side-bar h3 {
-    margin-left: 10px;
-    padding-top: 10px;
-    padding-bottom: 10px;
     text-align: center;
     color: aliceblue;
 }
@@ -87,9 +83,6 @@ a:hover{
     color: #2d3c4c;
 }
 
-.div-tipicos{
-    padding: 5px 0px 5px 20px;
-}
 
 .div-tipicos:hover{
     background-color: #485d73;
